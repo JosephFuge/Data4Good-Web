f7b85972078fcc5d5f69f3343155d11669e279c9
JosephFuge <josephfuge1@gmail.com>
2024-11-22 13:17:07 -0700

    Add more pages to work with Flask web server

--- app.py
+++ app.py
@@ -16,6 +16,32 @@ def home():
     key_phrases = rake.get_ranked_phrases()
     return render_template('index.html', data=key_phrases[0])
 
+@app.route('/journeymap')
+def journeymap():
+    # Extract key phrases
+    # rake.extract_keywords_from_text(text)
+
+    # Get the ranked key phrases
+    # key_phrases = rake.get_ranked_phrases()
+    return render_template('journeymap.html', current_journey_map="fake journey map testing")
+
+@app.route('/survey')
+def survey():
+    # Extract key phrases
+    # rake.extract_keywords_from_text(text)
+
+    # Get the ranked key phrases
+    # key_phrases = rake.get_ranked_phrases()
+    return render_template('survey.html')
+
+@app.route('/survey-results')
+def survey_results():
+    return render_template('survey-results.html')
+
+@app.route('/distribution')
+def distribution():
+    return render_template('distribution.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
 
